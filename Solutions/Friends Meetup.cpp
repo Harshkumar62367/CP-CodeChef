ecd26dce37100190c5fc9ff698530083c6b6bdf6
Harsh Kumar <72465090+Harshkumar62367@users.noreply.github.com>
2022-02-01 23:30:05 +0530

    Create Friends Meetup.cpp

---
+++ Solutions/Friends Meetup.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    if (a>=b)
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
