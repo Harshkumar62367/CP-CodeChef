b42947bb15126157475754227a5320bd6988c1fd
Harsh Kumar <72465090+Harshkumar62367@users.noreply.github.com>
2022-02-03 23:44:52 +0530

    Create Finding Square Roots-method_1.cpp

---
+++ Solutions/Finding Square Roots-method_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while (T--)
+	{
+	    int x;
+	    cin>>x;
+	    int result = sqrt(x);
+	    cout<<result<<endl;
+	}
+	return 0;
+}
